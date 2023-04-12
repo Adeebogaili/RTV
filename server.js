d7f480ccc505cd3328fce31239df51395512a154
Adeeb Ogaili <adeebnasser@gmail.com>
2023-04-12 09:32:17 -0500

    mongoose

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require('dotenv').config();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const { expressjwt } = require('express-jwt');
-const path = require('path');
+const path = require("path")
+
 
 const authRouter = require('./routes/authRouter.js');
 const issueRouter = require('./routes/issueRouter.js');
@@ -15,23 +16,15 @@ const commentRouter = require('./routes/commentRouter.js');
 
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 
 // Connect to MongoDB database
 
-const connectDatabase = async () => {
-  try {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.MONGO_URL);
-
-    console.log('connected to database');
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
-
-connectDatabase();
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.MONGO_URL, () =>
+  console.log('Connected to the DB')
+);
 
 // Define routes
 app.use('/auth', authRouter);
@@ -47,8 +40,8 @@ app.use((err, req, res, next) => {
   return res.send({ errMsg: err.message });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 // Start server
